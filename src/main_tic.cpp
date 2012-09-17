c2d9b34149ec8f2ac891dee04d4dc9fb12489d86
Pat Notz <patnotz@gmail.com>
2012-09-17 08:09:48 -0600

    migrating ideas from deps.sh shell script

--- src/main_tic.cpp
+++ src/main_tic.cpp
@@ -1,9 +1,11 @@
 // Copyright 2012 Patrick K. Notz
 #include <fstream>
 #include <string>
+#include <vector>
 #include <boost/format.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
+#include <boost/regex.hpp>
 #include <pstream.h>
 #include <tic_code.hpp>
 #include <tic_config.hpp>
@@ -20,13 +22,17 @@ int main(
   char* argv[]) {
   log() << "Version: " << version() << std::endl;
 
-  std::string filename;
+  std::string makefile;
+  std::string target;
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help,h", "produce help message")
-    ("file,f",
-	   po::value<std::string>(&filename)->default_value("lines.cpp"),
-	   "set file to read")
+    ("makefile,m",
+     po::value<std::string>(&makefile)->default_value("../aria_release_linux-gcc-4.4.4"),
+     "Makefile for parsing build commands")
+    ("target,t",
+     po::value<std::string>(&target)->default_value("Aria_Expression.o"),
+     "Target file to inspect")
 	 ;
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -37,21 +43,64 @@ int main(
     return 1;
   }
 
-  if( ! boost::filesystem::exists(filename) ) {
-    log() << boost::format("error: filename does not exist: %s") % filename << std::endl;
+  if( ! boost::filesystem::exists(makefile) ) {
+    log() << boost::format("error: makefile does not exist: %s") % makefile << std::endl;
     return 1;
   }
 
-  std::ifstream is(filename.c_str());
-  std::string str;
-  while (std::getline(is, str)) {
-    log() << str << std::endl;
+  std::ifstream i_makefile(makefile.c_str());
+  std::string comp_line;
+  boost::regex r_target(target + ":");
+  while (std::getline(i_makefile, comp_line)) {
+    if(boost::regex_search(comp_line.c_str(), r_target)) {
+      // The next comp_line contains the compilation command, with all the yummy -D and -I flags
+      std::getline(i_makefile, comp_line);
+      log() << comp_line << std::endl;
+      break;
+    }
   }
+  i_makefile.close();
 
-  redi::pstream in("ls -ld " + filename);
+  std::vector<std::string> include_paths;
+  include_paths.reserve(128);
+  boost::regex r_path("-I\"(.*?)\"");
+  for(boost::sregex_iterator i(comp_line.begin(), comp_line.end(), r_path), end; i != end; ++i) {
+    const std::string path = (*i)[1];
+    bool valid = boost::filesystem::is_directory(path);
+    if(valid)
+      include_paths.push_back(path);
+    else
+      log() << "invalid path: " << path << std::endl;
+  }
+  log() << boost::format("found %d valid include paths") % include_paths.size() << std::endl;
+
+  boost::regex r_src("^(.*)\\.o$");
+  boost::cmatch m_src;
+  std::string source;
+  if(boost::regex_match(target.c_str(), m_src, r_src)) {
+    source = "/Users/pknotz/workspace/code/aria/src/" + m_src[1] + ".C";
+  }
+  log() << "source file: " << source << std::endl;
+
+  std::ifstream i_source(source.c_str());
+  boost::regex r_include("#include [<\"](.*)[>\"]");
+  boost::cmatch m_include;
+  std::vector<std::string> includes;
+  std::string src_line;
+  while (std::getline(i_source, src_line)) {
+    if(boost::regex_match(src_line.c_str(), m_include, r_include)) {
+      log() << "found " << m_include[1] << std::endl;
+      includes.push_back(m_include[1]);
+    }
+  }
+  i_source.close();
+
+  /*
+  redi::pstream in("ls -ld " + makefile);
   while (std::getline(in, str)) {
     log() << str << std::endl;
   }
+  */
 
   return 0;
 }
